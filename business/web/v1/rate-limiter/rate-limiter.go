99dde21938952cda4a8b377804b2eb06cdfb7027
Sekiranda <sekirandahamza@gmail.com>
2023-12-13 13:43:29 +0300

    progress with token bucket

--- business/web/v1/rate-limiter/rate-limiter.go
+++ business/web/v1/rate-limiter/rate-limiter.go
@@ -1,22 +1,41 @@
-package rate_limiter
+package ratelimiter
+
+import (
+	"github.com/Zanda256/rate-limiter-go/business/web/v1/rate-limiter/tokenbucket"
+	"github.com/Zanda256/rate-limiter-go/foundation/cache"
+)
+
+// DefaultRateLimitPeriod Default Rate Limit Period in seconds
+var DefaultRateLimitPeriod = 30
+var DefaultRateLimitCapacity = 5
 
 // Write the supported types of rate limiting and their implementations in this file
 // Start with Leaky bucket
+type RateLimiterImpl struct {
+	tokenbucket.BucketController
+}
+type RateLimiter interface {
+	Accept(string) bool
+}
 
 type Algo int
 
 const (
-	LeakyBucket = iota
-	TokenBucket
-	FixedWindow
-	SlidingLog
-	SlidingWindow
+	LeakyBucket   = "LeakyBucket"
+	TokenBucket   = "TokenBucket"
+	FixedWindow   = "FixedWindow"
+	SlidingLog    = "SlidingLog"
+	SlidingWindow = "SlidingWindow"
 )
 
-var supportedAlgos = map[string]int{
-	"LeakyBucket":   LeakyBucket,
-	"TokenBucket":   TokenBucket,
-	"FixedWindow":   FixedWindow,
-	"SlidingLog":    SlidingLog,
-	"SlidingWindow": SlidingWindow,
+type RateLimiterConfig struct {
+	kvStore *cache.RedisCache
+}
+
+func NewRateLimiter(cfg RateLimiterConfig) RateLimiter {
+	return tokenbucket.NewBucketController(tokenbucket.BucketControllerConfig{
+		Store:    cfg.kvStore,
+		Period:   DefaultRateLimitPeriod,
+		Capacity: DefaultRateLimitCapacity,
+	})
 }
